40a9e179480b82fb4471155d65a72623dade5e22
olgngy <olganagy29@gmail.com>
2015-05-31 23:55:45 +0100

    Fix whitespaces

--- common.php
+++ common.php
@@ -1,35 +1,35 @@
 <?php
 
-  $dbhost = 'localhost';
-  $dbname = 'spellvocabulary';
-  $dbuser = 'spellvocabulary';
-  $dbpass = 'setparam1';
+$dbhost = 'localhost';
+$dbname = 'spellvocabulary';
+$dbuser = 'spellvocabulary';
+$dbpass = 'setparam1';
 
-  $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-  if ($connection->connect_error) {
-    die ($connection->connect_error);
-  }
+$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error) {
+  die ($connection->connect_error);
+}
 
-  function createTable($name, $columns) {
-    queryMysql("CREATE TABLE IF NOT EXISTS $name($columns)");
-    echo "Table '$name' created or already exists. <br>";
-  }
+function createTable($name, $columns) {
+  queryMysql("CREATE TABLE IF NOT EXISTS $name($columns)");
+  echo "Table '$name' created or already exists. <br>";
+}
 
-  function queryMysql($query) {
-    global $connection;
-    $result = $connection->query($query);
-    if (!$result) {
-      die ($connection->error);
-    }
-    return $result;
+function queryMysql($query) {
+  global $connection;
+  $result = $connection->query($query);
+  if (!$result) {
+    die ($connection->error);
   }
-  
-  function sanitizeString($str) {
-    global $connection;
-    $str = strip_tags($str);
-    $str = htmlentities($str);
-    $str = stripslashes($str);
-    return $connection->real_escape_string($str);
-  }
+  return $result;
+}
+
+function sanitizeString($str) {
+  global $connection;
+  $str = strip_tags($str);
+  $str = htmlentities($str);
+  $str = stripslashes($str);
+  return $connection->real_escape_string($str);
+}
 
 ?>
